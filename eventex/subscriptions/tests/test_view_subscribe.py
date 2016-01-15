935d822b6ab0696cfb845239df61571d3ddfe592
kureck <kureck@gmail.com>
2016-01-15 08:05:31 -0200

    adds details

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -45,7 +45,7 @@ class SubscribePostValid(TestCase):
 
     def test_post(self):
         """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -75,11 +75,3 @@ class SubscribePostInvalid(TestCase):
     def test_doesnt_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Eric Kureck', cpf='12345678901',
-                    email='kureck@gmail.com', phone='11-98500-5433')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscricao realizada com sucesso!')
