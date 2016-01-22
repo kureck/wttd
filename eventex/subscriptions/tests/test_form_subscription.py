ac2fe1d5a8592e7d151564e5163b5b3f5c8b362c
kureck <kureck@gmail.com>
2016-01-22 07:22:37 -0200

    adds capitalized names

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -21,6 +21,11 @@ class SubscriptionFormTest(TestCase):
 
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized."""
+        form = self.make_validated_form(name='ERIC Kureck')
+        self.assertEqual('Eric Kureck', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
