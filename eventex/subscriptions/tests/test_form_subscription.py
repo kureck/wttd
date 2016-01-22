f3aed2f04d9229a504c45cba7a040f090d17f1d1
kureck <kureck@gmail.com>
2016-01-22 07:04:43 -0200

    adds cpf validation

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -3,10 +3,29 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
         """Form must have 4 fields."""
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertSequenceEqual(expected, list(self.form.fields))
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only accept digit"""
+        form = self.make_validated_form(cpf='ABCD5678901')
+
+        self.assertListEqual(['cpf'], list(form.errors))
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits"""
+        form = self.make_validated_form(cpf='1234')
+
+        self.assertListEqual(['cpf'], list(form.errors))
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Eric Kureck', cpf='12345678901',
+                     email='kureck@gmail.com', phone='11-98500-5433')
+
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
